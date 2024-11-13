06ae72033e84e1ac33166bbd98448e74e88ada56
Karl Johan Grahn <karl.johan@stakater.com>
2024-11-13 21:38:43 +0100

    update

--- prepare_theme.sh
+++ prepare_theme.sh
@@ -2,6 +2,8 @@ chmod +x theme_common/scripts/create-virtual-env.sh
 ./theme_common/scripts/create-virtual-env.sh
 source venv/bin/activate
 pip3 install -r theme_common/requirements.txt
-python3 theme_common/scripts/combine_theme_resources.py theme_common/resources theme_override/resources dist/_theme
+python3 theme_common/scripts/combine_theme_resources.py -s theme_common/resources -ov theme_override/resources -o dist/_theme
+# The next step is used to override resources for pull request builds - these overrides could as well have been put in the local theme_override folder, but this is a generic solution
+python3 theme_common/scripts/combine_theme_resources.py -s theme_common/resources_pr_specific -ov theme_override/resources -o dist/_theme -skiprmtree
 python3 theme_common/scripts/combine_mkdocs_config_yaml.py theme_common/mkdocs.yml theme_override/mkdocs.yml mkdocs.yml
 deactivate
